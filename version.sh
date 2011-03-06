1dbdb33c80c5f19910576f47b19a10c4bba6327f
Ilmari Heikkinen <ilmari@banigold.(none)>
2011-03-06 20:41:43 +0200

    version.sh: fix for BSD date

--- version.sh
+++ version.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo `date -I`.`git log --format='%ai' | awk '{print $1}' | sort | uniq | wc -l|sed 's/\s//g'`.`git log --format='%ai' | grep $(date -I) | wc -l|sed 's/\s//g'`
+echo `date +%Y-%m-%d`.`git log --format='%ai' | awk '{print $1}' | sort | uniq | wc -l|sed 's/\s//g'`.`git log --format='%ai' | grep $(date +%Y-%m-%d) | wc -l|sed 's/\s//g'`
