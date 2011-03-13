faa816d26b034869ef22e397ce1d389110004e3f
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2011-03-13 06:12:20 -0700

    upload_util.js: ImgUR uploader from paulrouget

--- src/upload_util.js
+++ src/upload_util.js
@@ -312,3 +312,33 @@ DnDUpload = Klass({
     }, false);
   }
 });
+
+ImgUR = {
+  upload : function(file) {
+
+    // file is from a <input> tag or from Drag'n Drop
+    // Is the file an image?
+
+    if (!file || !file.type.match(/^image.*/)) return;
+
+    // It is!
+    // Let's build a FormData object
+
+    var fd = new FormData();
+    fd.append("image", file); // Append the file
+    fd.append("key", "6528448c258cff474ca9701c5bab6927");
+    // Get your own key: http://api.imgur.com/
+
+    // Create the XHR (Cross-Domain XHR FTW!!!)
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://api.imgur.com/2/upload.json"); // Boooom!
+    xhr.onload = function() {
+        // Big win!
+        // The URL of the image is:
+        JSON.parse(xhr.responseText).upload.links.imgur_page;
+    }
+     // Ok, I don't handle the errors. An exercice for the reader.
+     // And now, we send the formdata
+     xhr.send(fd);
+  }
+};
