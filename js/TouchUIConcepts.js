6e34f63642a9873402db77c527387ecd0f960b78
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2015-12-18 11:10:23 +0000

    refactor touch prototype

--- js/TouchUIConcepts.js
+++ js/TouchUIConcepts.js
@@ -1,306 +1,93 @@
 (function() {
 
-	var pixelRatio = window.devicePixelRatio;
-
-
-	var toColor = function(c) {
-		return 'rgb('+(c[0]|0)+','+(c[1]|0)+','+(c[2]|0)+')'
+	var App = function() {
+		this.init();
 	};
 
-	var blend = function(a, b, f) {
-		return [
-			(1-f)*a[0] + f*b[0],
-			(1-f)*a[1] + f*b[1],
-			(1-f)*a[2] + f*b[2]
-		];
-	};
+	App.prototype.init = function() {
+		this.pixelRatio = window.devicePixelRatio;
+		this.mode = App.Mode.DRAW;
 
-	
-	var canvas = document.createElement('canvas');
-	canvas.width = window.innerWidth * pixelRatio;
-	canvas.height = window.innerHeight * pixelRatio;
-	canvas.id = 'brush-canvas';
+		this.ticker = this.tick.bind(this);
 
-	document.body.appendChild(canvas);
-
-	var ctx = canvas.getContext('2d');
-
-	var drawCanvas = document.createElement('canvas');
-	drawCanvas.width = window.innerWidth * pixelRatio;
-	drawCanvas.height = window.innerHeight * pixelRatio;
-	drawCanvas.id = 'draw-canvas';
-
-	document.body.appendChild(drawCanvas);
-
-	var drawCtx = drawCanvas.getContext('2d');
-	drawCtx.fillStyle = '#fff';
-	drawCtx.fillRect(0, 0, drawCanvas.width, drawCanvas.height);
-	
-	var strokeCanvas = document.createElement('canvas');
-	strokeCanvas.width = window.innerWidth * pixelRatio;
-	strokeCanvas.height = window.innerHeight * pixelRatio;
-	strokeCanvas.id = 'stroke-canvas';
-
-	document.body.appendChild(strokeCanvas);
-
-	var strokeCtx = strokeCanvas.getContext('2d');
-	strokeCtx.fillStyle = '#fff';
-	strokeCtx.fillRect(0, 0, strokeCanvas.width, strokeCanvas.height);
-
-	var Mode = {
-		DRAW: 0,
-		BRUSH_RESIZE: 1,
-		OPACITY_CHANGE: 2,
-		COLOR_PICKER: 3
-	};
-
-	var mode = Mode.DRAW;
-
-	var brush = {
-		x: 0,
-		y: 0,
-		r: 3,
-		opacity: 1,
-		blend: 0.5,
-		color: '#ff0000',
-		colorArray: [255,0,0,255]
-	};
-
-	var modeToggle = function(toggle, targetMode) {
-		var toggleCanvas = document.createElement('canvas');
-		var bbox = toggle.getBoundingClientRect(); 
-		var w = bbox.width;
-		var h = bbox.height;
-		toggleCanvas.width = w * pixelRatio;
-		toggleCanvas.height = h * pixelRatio;
-		toggleCanvas.style.width = w + 'px';
-		toggleCanvas.style.height = h + 'px';
-
-		toggle.appendChild(toggleCanvas);
-
-		var toggleCtx = toggleCanvas.getContext('2d');
-		toggleCtx.scale(pixelRatio, pixelRatio);
-
-		var update = function() {
-			toggleCtx.clearRect(0, 0, w, h);
-
-			if (targetMode === Mode.COLOR_PICKER) {
-				toggleCtx.fillStyle = brush.color;
-				toggleCtx.fillRect(0, 0, w, h);
-
-			} else if (targetMode === Mode.BRUSH_RESIZE) {
-
-				var r = Math.min(brush.r, h/2 - 15)
-
-				toggleCtx.beginPath();
-				toggleCtx.arc(w/2, h/2-5, r, 0, Math.PI*2, true);
-				toggleCtx.fillStyle = 'white';
-				toggleCtx.fill();
-
-				toggleCtx.fillStyle = 'black';
-				if (r < brush.r) {
-					toggleCtx.fillRect(w/2-2, h/2-5, 5, 1);
-					toggleCtx.fillRect(w/2, h/2-5-2, 1, 5);
-				}
-
-				if (brush.r < 1) {
-					var rs = (Math.round(brush.r * 100) / 100).toString().replace(/(\...).+/, '$1');
-				} else {
-					var rs = Math.round(brush.r).toString();
-				}
-				var tw = toggleCtx.measureText(rs).width;
-				toggleCtx.fillText(rs, w/2-tw/2, h-2);
-
-			} else if (targetMode === Mode.OPACITY_CHANGE) {
-				var segs = 8;
-				var pw = w/segs;
-				var ph = h/segs;
-				toggleCtx.globalAlpha = 1;
-				for (var x=0; x<segs; x++) {
-					for (var y=0; y<segs; y++) {
-						toggleCtx.fillStyle = ((x+y) % 2) ? '#AAA' : '#CCC';
-						toggleCtx.fillRect(x * pw, y * ph, pw, ph);
-					}
-				}
-				toggleCtx.fillStyle = 'black';
-				toggleCtx.globalAlpha = brush.opacity;
-				toggleCtx.fillRect(0, 0, w, h);
-			}
+		this.brush = {
+			x: 0,
+			y: 0,
+			r: 3,
+			opacity: 1,
+			blend: 0.5,
+			color: '#ff0000',
+			colorArray: [255,0,0,255]
 		};
-		update();
 
-		toggle.addEventListener('touchstart', function(ev) {
-			ev.preventDefault();
+		App.modeToggle(this, window.brushResize, App.Mode.BRUSH_RESIZE);
+		App.modeToggle(this, window.opacityChange, App.Mode.OPACITY_CHANGE);
+		App.modeToggle(this, window.colorPicker, App.Mode.COLOR_PICKER);
 
-			this.startRadius = brush.r;
-			this.startOpacity = brush.opacity;
-			this.startColor = brush.color;
-			this.startColorArray = brush.colorArray;
-
-			this.startX = ev.touches[0].clientX;
-			this.startY = ev.touches[0].clientY;
-
-			mode = targetMode;
-		}, false);
-
-		toggle.addEventListener('touchend', function(ev) {
-			ev.preventDefault();
-			mode = Mode.DRAW;
-		}, false);
-
-		toggle.addEventListener('touchcancel', function(ev) {
-			ev.preventDefault();
-			brush.r = this.startRadius;
-			brush.opacity = this.startOpacity;
-			brush.color = this.startColor;
-			brush.colorArray = this.startColorArray;
-			mode = Mode.DRAW;
-		}, false);
-
-		toggle.addEventListener('touchmove', function(ev) {
-			ev.preventDefault();
-			switch (mode) {
-				case Mode.BRUSH_RESIZE: {
-					var dx = ev.touches[0].clientX - this.startX;
-					var dy = ev.touches[0].clientY - this.startY;
-					var d = Math.sqrt(dx*dx + dy*dy);
-					brush.r = Math.max(0.25, this.startRadius + dx);
-					break;
-				}
-				case Mode.OPACITY_CHANGE: {
-					var dx = ev.touches[0].clientX - this.startX;
-					var dy = ev.touches[0].clientY - this.startY;
-					var d = Math.sqrt(dx*dx + dy*dy);
-					brush.opacity = Math.max(0, Math.min(1, this.startOpacity - dy/50));
-					break;
-				}
-				case Mode.COLOR_PICKER: {
-					var x = Math.floor(ev.touches[0].clientX * pixelRatio);
-					var y = Math.floor(ev.touches[0].clientY * pixelRatio);
-					var c = drawCtx.getImageData(x, y, 1, 1);
-					brush.color = toColor(c.data);
-					brush.colorArray = c.data;
-					break;
-				}
-			}
-
-			update();
-		}, false);
+		this.setupCanvas();
+		this.addEventListeners();
 	};
 
-	modeToggle(window.brushResize, Mode.BRUSH_RESIZE);
-	modeToggle(window.opacityChange, Mode.OPACITY_CHANGE);
-	modeToggle(window.colorPicker, Mode.COLOR_PICKER);
 
-	var eventHandler = {
-		handleEvent: function(ev) {
-			ev.preventDefault();
-			if (this[ev.type]) {
-				this[ev.type](ev);
-			}
-		},
 
-		startX: 0,
-		startY: 0,
-		startRadius: 0,
-		startOpacity: 1,
-		startColor: '#ff0000',
-		touchActive: false,
+	App.prototype.setupCanvas = function() {
+		var canvas = document.createElement('canvas');
+		canvas.width = window.innerWidth * this.pixelRatio;
+		canvas.height = window.innerHeight * this.pixelRatio;
+		canvas.id = 'brush-canvas';
 
-		resetMode: function() {
-			this.touchActive = false;
-			if (mode !== Mode.DRAW) {
-				mode = Mode.DRAW;
-				var toggles = document.querySelectorAll('.mode-toggle');
-				for (var i = 0; i < toggles.length; i++) {
-					toggles[i].classList.remove('active');
-				}
-			}
-		},
+		document.body.appendChild(canvas);
 
-		log: function(txt) {
-			drawCtx.fillStyle = '#fff';
-			drawCtx.fillRect(0,0, 200, 50);
-			drawCtx.fillStyle = '#000';
-			drawCtx.font = '40px sans-serif';
-			drawCtx.fillText(txt, 5, 40);
-		},
+		var ctx = canvas.getContext('2d');
 
-		touchstart: function(ev) {
-			this.touchActive = true;
-			this.startRadius = brush.r;
-			this.startOpacity = brush.opacity;
-			this.startColor = brush.color;
-			this.startColorArray = brush.colorArray;
+		var drawCanvas = document.createElement('canvas');
+		drawCanvas.width = window.innerWidth * this.pixelRatio;
+		drawCanvas.height = window.innerHeight * this.pixelRatio;
+		drawCanvas.id = 'draw-canvas';
 
-			this.startX = ev.touches[0].clientX;
-			this.startY = ev.touches[0].clientY;
-			brush.x = ev.touches[0].clientX;
-			brush.y = ev.touches[0].clientY;
-			brush.pressure = ev.touches[0].force;
+		document.body.appendChild(drawCanvas);
 
-			if (mode === Mode.BRUSH_RESIZE) {
-				brush.x += -50;
-				brush.y += -50;
-			} else if (mode === Mode.OPACITY_CHANGE) {
-				brush.x += -50;
-				brush.y += -50;
-			} else if (mode === Mode.COLOR_PICKER) {
-				var x = Math.floor(brush.x * pixelRatio);
-				var y = Math.floor(brush.y * pixelRatio);
-				var c = drawCtx.getImageData(x, y, 1, 1);
-				brush.color = toColor(c.data);
-				brush.colorArray = c.data;
-			} else if (mode === Mode.DRAW) {
-				brush.blend = 1-ev.touches[0].force;
-				drawBrush(true);
-			}
-		},
+		var drawCtx = drawCanvas.getContext('2d');
+		drawCtx.fillStyle = '#fff';
+		drawCtx.fillRect(0, 0, drawCanvas.width, drawCanvas.height);
+		
+		var strokeCanvas = document.createElement('canvas');
+		strokeCanvas.width = window.innerWidth * this.pixelRatio;
+		strokeCanvas.height = window.innerHeight * this.pixelRatio;
+		strokeCanvas.id = 'stroke-canvas';
 
-		touchend: function(ev) {
-			if (mode === Mode.DRAW) {
-				endDrawBrush();
-			}
-			this.resetMode();
-		},
+		document.body.appendChild(strokeCanvas);
 
-		touchcancel: function(ev) {
-			this.resetMode();
-			brush.r = this.startRadius;
-			brush.opacity = this.startOpacity;
-			brush.color = this.startColor;
-			brush.colorArray = this.startColorArray;
-		},
+		var strokeCtx = strokeCanvas.getContext('2d');
+		strokeCtx.fillStyle = '#fff';
+		strokeCtx.fillRect(0, 0, strokeCanvas.width, strokeCanvas.height);
 
-		touchmove: function(ev) {
-			brush.pressure = ev.touches[0].force;
-			if (mode === Mode.DRAW) {
-				brush.x = ev.touches[0].clientX;
-				brush.y = ev.touches[0].clientY;
-				brush.blend = 1-ev.touches[0].force;
-				drawBrush(false);
-			}
-		}
+		this.drawCtx = drawCtx;
+		this.ctx = ctx;
+		this.strokeCtx = strokeCtx;
+		this.canvas = canvas;
+		this.strokeCanvas = strokeCanvas;
+		this.drawCanvas = drawCanvas;
+	};
+	
+	App.prototype.addEventListeners = function() {
+		this.eventHandler = new App.EventHandler(this, this.canvas);
 	};
 
-	canvas.addEventListener("touchstart", eventHandler, false);
-	canvas.addEventListener("touchend", eventHandler, false);
-	canvas.addEventListener("touchcancel", eventHandler, false);
-	canvas.addEventListener("touchmove", eventHandler, false);
-
-	var drawBrush = function(newStroke) {
-		var ctx = strokeCtx;
+	App.prototype.drawBrush = function(newStroke) {
+		var ctx = this.strokeCtx;
+		var brush = this.brush;
 
 		ctx.save(); {
 
-			ctx.scale(pixelRatio, pixelRatio);
+			ctx.scale(this.pixelRatio, this.pixelRatio);
 
 			ctx.save(); {
 				ctx.fillStyle = brush.color;
 				ctx.globalAlpha = brush.opacity;
 				if (newStroke) {
-					// var x = Math.floor(brush.x * pixelRatio);
-					// var y = Math.floor(brush.y * pixelRatio);
+					// var x = Math.floor(brush.x * app.pixelRatio);
+					// var y = Math.floor(brush.y * app.pixelRatio);
 					// var c = drawCtx.getImageData(x, y, 1, 1);
 					// ctx.beginPath();
 					// ctx.fillStyle = toColor
@@ -315,8 +102,8 @@
 					var rdy = dy / d;
 					var rdp = dp / d;
 					while (d > 0) {
-						var x = Math.floor((brush.lastX + d*rdx) * pixelRatio);
-						var y = Math.floor((brush.lastY + d*rdy) * pixelRatio);
+						var x = Math.floor((brush.lastX + d*rdx) * this.pixelRatio);
+						var y = Math.floor((brush.lastY + d*rdy) * this.pixelRatio);
 						// var c = drawCtx.getImageData(x, y, 1, 1);
 						// ctx.fillStyle = toColor(blend(brush.colorArray, c.data, brush.blend));
 						ctx.beginPath();
@@ -334,21 +121,26 @@
 		brush.lastPressure = brush.pressure;
 	};
 
-	var endDrawBrush = function() {
-		var ctx = strokeCtx;
-		drawCtx.drawImage(strokeCanvas, 0, 0);
+	App.prototype.endDrawBrush = function() {
+		var ctx = this.strokeCtx;
+		this.drawCtx.drawImage(ctx.canvas, 0, 0);
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);			
 	};
 
-	var tick = function() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		if (mode !== Mode.DRAW && eventHandler.touchActive) {
+	App.prototype.tick = function() {
+		var ctx = this.ctx;
+		var brush = this.brush;
+		var mode = this.mode;
+		var pixelRatio = this.pixelRatio;
+
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		if (mode !== App.Mode.DRAW && this.eventHandler.touchActive) {
 			ctx.save(); {
 
 				ctx.scale(pixelRatio, pixelRatio);
 
 				ctx.save(); {
-					if (mode === Mode.COLOR_PICKER) {
+					if (mode === App.Mode.COLOR_PICKER) {
 						ctx.beginPath();
 						ctx.fillStyle = brush.color;
 						ctx.fillRect(brush.x-60, brush.y-60, 20, 20);
@@ -362,7 +154,7 @@
 						ctx.arc(brush.x, brush.y, brush.r, 0, Math.PI*2, true);
 						ctx.strokeStyle = '#fff';
 						ctx.stroke();
-						if (mode === Mode.OPACITY_CHANGE) {
+						if (mode === App.Mode.OPACITY_CHANGE) {
 							ctx.globalAlpha = brush.opacity;
 							ctx.fillStyle = brush.color;
 							ctx.fill();
@@ -373,8 +165,262 @@
 			} ctx.restore();
 		}
 
-		window.requestAnimationFrame(tick);
+		window.requestAnimationFrame(this.ticker);
 	};
 
-	tick();
+
+
+
+
+
+	App.toColor = function(c) {
+		return 'rgb('+(c[0]|0)+','+(c[1]|0)+','+(c[2]|0)+')';
+	};
+
+	App.blend = function(a, b, f) {
+		return [
+			(1-f)*a[0] + f*b[0],
+			(1-f)*a[1] + f*b[1],
+			(1-f)*a[2] + f*b[2]
+		];
+	};
+
+	App.modeToggle = function(app, toggle, targetMode) {
+		var toggleCanvas = document.createElement('canvas');
+		var bbox = toggle.getBoundingClientRect(); 
+		var w = bbox.width;
+		var h = bbox.height;
+		toggleCanvas.width = w * app.pixelRatio;
+		toggleCanvas.height = h * app.pixelRatio;
+		toggleCanvas.style.width = w + 'px';
+		toggleCanvas.style.height = h + 'px';
+
+		toggle.appendChild(toggleCanvas);
+
+		var toggleCtx = toggleCanvas.getContext('2d');
+		toggleCtx.scale(app.pixelRatio, app.pixelRatio);
+
+		var update = function() {
+			toggleCtx.clearRect(0, 0, w, h);
+
+			if (targetMode === App.Mode.COLOR_PICKER) {
+				toggleCtx.fillStyle = app.brush.color;
+				toggleCtx.fillRect(0, 0, w, h);
+
+			} else if (targetMode === App.Mode.BRUSH_RESIZE) {
+
+				var r = Math.min(app.brush.r, h/2 - 15)
+
+				toggleCtx.beginPath();
+				toggleCtx.arc(w/2, h/2-5, r, 0, Math.PI*2, true);
+				toggleCtx.fillStyle = 'white';
+				toggleCtx.fill();
+
+				toggleCtx.fillStyle = 'black';
+				if (r < app.brush.r) {
+					toggleCtx.fillRect(w/2-2, h/2-5, 5, 1);
+					toggleCtx.fillRect(w/2, h/2-5-2, 1, 5);
+				}
+
+				if (app.brush.r < 1) {
+					var rs = (Math.round(app.brush.r * 100) / 100).toString().replace(/(\...).+/, '$1');
+				} else {
+					var rs = Math.round(app.brush.r).toString();
+				}
+				var tw = toggleCtx.measureText(rs).width;
+				toggleCtx.fillText(rs, w/2-tw/2, h-2);
+
+			} else if (targetMode === App.Mode.OPACITY_CHANGE) {
+				var segs = 8;
+				var pw = w/segs;
+				var ph = h/segs;
+				toggleCtx.globalAlpha = 1;
+				for (var x=0; x<segs; x++) {
+					for (var y=0; y<segs; y++) {
+						toggleCtx.fillStyle = ((x+y) % 2) ? '#AAA' : '#CCC';
+						toggleCtx.fillRect(x * pw, y * ph, pw, ph);
+					}
+				}
+				toggleCtx.fillStyle = 'black';
+				toggleCtx.globalAlpha = app.brush.opacity;
+				toggleCtx.fillRect(0, 0, w, h);
+			}
+		};
+		update();
+
+		toggle.addEventListener('touchstart', function(ev) {
+			ev.preventDefault();
+
+			this.startRadius = app.brush.r;
+			this.startOpacity = app.brush.opacity;
+			this.startColor = app.brush.color;
+			this.startColorArray = app.brush.colorArray;
+
+			this.startX = ev.touches[0].clientX;
+			this.startY = ev.touches[0].clientY;
+
+			app.mode = targetMode;
+		}, false);
+
+		toggle.addEventListener('touchend', function(ev) {
+			ev.preventDefault();
+			app.mode = App.Mode.DRAW;
+		}, false);
+
+		toggle.addEventListener('touchcancel', function(ev) {
+			ev.preventDefault();
+			app.brush.r = this.startRadius;
+			app.brush.opacity = this.startOpacity;
+			app.brush.color = this.startColor;
+			app.brush.colorArray = this.startColorArray;
+			app.mode = App.Mode.DRAW;
+		}, false);
+
+		toggle.addEventListener('touchmove', function(ev) {
+			ev.preventDefault();
+			var mode = app.mode;
+			switch (mode) {
+				case App.Mode.BRUSH_RESIZE: {
+					var dx = ev.touches[0].clientX - this.startX;
+					var dy = ev.touches[0].clientY - this.startY;
+					var d = Math.sqrt(dx*dx + dy*dy);
+					app.brush.r = Math.max(0.25, this.startRadius + dx);
+					break;
+				}
+				case App.Mode.OPACITY_CHANGE: {
+					var dx = ev.touches[0].clientX - this.startX;
+					var dy = ev.touches[0].clientY - this.startY;
+					var d = Math.sqrt(dx*dx + dy*dy);
+					app.brush.opacity = Math.max(0, Math.min(1, this.startOpacity - dy/50));
+					break;
+				}
+				case App.Mode.COLOR_PICKER: {
+					var x = Math.floor(ev.touches[0].clientX * app.pixelRatio);
+					var y = Math.floor(ev.touches[0].clientY * app.pixelRatio);
+					var c = app.drawCtx.getImageData(x, y, 1, 1);
+					app.brush.color = App.toColor(c.data);
+					app.brush.colorArray = c.data;
+					break;
+				}
+			}
+
+			update();
+		}, false);
+	};
+
+
+
+
+
+	App.Mode = {
+		DRAW: 0,
+		BRUSH_RESIZE: 1,
+		OPACITY_CHANGE: 2,
+		COLOR_PICKER: 3
+	};
+
+
+
+
+	App.EventHandler = function(app, el) {
+		this.app = app;
+
+		this.startX = 0;
+		this.startY = 0;
+		this.startRadius = 0;
+		this.startOpacity = 1;
+		this.startColor = '#ff0000';
+		this.touchActive = false;
+
+		el.addEventListener("touchstart", this, false);
+		el.addEventListener("touchend", this, false);
+		el.addEventListener("touchcancel", this, false);
+		el.addEventListener("touchmove", this, false);
+	};
+
+	App.EventHandler.prototype = {
+		handleEvent: function(ev) {
+			ev.preventDefault();
+			if (this[ev.type]) {
+				this[ev.type](ev);
+			}
+		},
+
+		resetMode: function() {
+			this.touchActive = false;
+			if (this.app.mode !== App.Mode.DRAW) {
+				this.app.mode = App.Mode.DRAW;
+				var toggles = document.querySelectorAll('.mode-toggle');
+				for (var i = 0; i < toggles.length; i++) {
+					toggles[i].classList.remove('active');
+				}
+			}
+		},
+
+		log: function(txt) {
+			var drawCtx = this.app.drawCtx;
+			drawCtx.fillStyle = '#fff';
+			drawCtx.fillRect(0,0, drawCtx.canvas.width, 50);
+			drawCtx.fillStyle = '#000';
+			drawCtx.font = '40px sans-serif';
+			drawCtx.fillText(txt, 5, 40);
+		},
+
+		touchstart: function(ev) {
+			this.touchActive = true;
+			this.startRadius = this.app.brush.r;
+			this.startOpacity = this.app.brush.opacity;
+			this.startColor = this.app.brush.color;
+			this.startColorArray = this.app.brush.colorArray;
+
+			this.startX = ev.touches[0].clientX;
+			this.startY = ev.touches[0].clientY;
+			this.app.brush.x = ev.touches[0].clientX;
+			this.app.brush.y = ev.touches[0].clientY;
+			this.app.brush.pressure = ev.touches[0].force;
+
+			if (this.app.mode === App.Mode.COLOR_PICKER) {
+				var x = Math.floor(this.app.brush.x * this.app.pixelRatio);
+				var y = Math.floor(this.app.brush.y * this.app.pixelRatio);
+				var c = this.app.drawCtx.getImageData(x, y, 1, 1);
+				this.app.brush.color = App.toColor(c.data);
+				this.app.brush.colorArray = c.data;
+			} else if (this.app.mode === App.Mode.DRAW) {
+				this.app.brush.blend = 1-ev.touches[0].force;
+				this.app.drawBrush(true);
+			}
+		},
+
+		touchend: function(ev) {
+			if (this.app.mode === App.Mode.DRAW) {
+				this.app.endDrawBrush();
+			}
+			this.resetMode();
+		},
+
+		touchcancel: function(ev) {
+			this.resetMode();
+			this.app.brush.r = this.startRadius;
+			this.app.brush.opacity = this.startOpacity;
+			this.app.brush.color = this.startColor;
+			this.app.brush.colorArray = this.startColorArray;
+		},
+
+		touchmove: function(ev) {
+			this.app.brush.pressure = ev.touches[0].force;
+			if (this.app.mode === App.Mode.DRAW) {
+				this.app.brush.x = ev.touches[0].clientX;
+				this.app.brush.y = ev.touches[0].clientY;
+				this.app.brush.blend = 1-ev.touches[0].force;
+				this.app.drawBrush(false);
+			}
+		}
+	};
+
+
+
+
+
+	var app = new App();
+	app.tick();
 })();
