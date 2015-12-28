ac164de880a012b8959c65ae37be01b039398c2a
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2015-12-28 04:33:54 +0200

    fix watery preset

--- js/TouchUIConcepts.js
+++ js/TouchUIConcepts.js
@@ -489,7 +489,7 @@
 		pencilOpacity: [0, 0, 0.9, 0.0, 1, 1, 1, 1],
 		pencilRadius: [0, 0.5, 0, 0.65, 0.8, 1, 1, 1],
 
-		wateryOpacity: [0, 0, 0, 1, 0, 1, 1, 0]
+		wateryOpacity: [0, 0, 0, 1, 0, 1, 1, 0.1]
 	};
 
 	App.CurvePresets = {
